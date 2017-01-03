3fd9aebecf10a76cb3003ed8d2787c26a2d06451
Raphael Ohanian <raphael.ohanian@telecomnancy.eu>
2017-01-03 16:28:27 +0100

    Correction de bugs

--- shazam_ohs/TOP/src/Final.scala
+++ shazam_ohs/TOP/src/Final.scala
@@ -59,7 +59,7 @@ object Final {
     for (i <- 0 to (ni - 2)) {
       var temp: Array[Int] = Array()
       for (k <- 0 to 1024 - 1) {
-        temp = temp ++ Array(t(i + k))
+        temp = temp ++ Array(t(i*1024 + k))
       }
       res = res ++ Array(temp)
     }
@@ -158,19 +158,19 @@ object Final {
   def fftm(tab: Array[Array[Int]]): Array[Array[Double]] = {
     var n: Int = tab.length - 1
     var res: Array[Array[Double]] = Array(Array())
-    res(0) = ItoD(tab(0))  
-    println("Veuillez patientez.. Etape 1 sur 3")
+    res(0) = ItoD(tab(0))
+    println("Veuillez patientez.. Etape 1 sur 2")
     for (i <- 1 to n) {
       res = res ++ Array(fftr(fft(ReToIm(tab(i)))))
     }
-    println("Veuillez patientez.. Etape 2 sur 3")
+    println("Veuillez patientez.. Etape 2 sur 2")
     return res
   }
-  
-  def ItoD(tab : Array[Int]) : Array[Double] = {
-    var res : Array[Double] = Array()
-    for (u<-0 to tab.length -1) {
-      var temp : Double = tab(u)
+
+  def ItoD(tab: Array[Int]): Array[Double] = {
+    var res: Array[Double] = Array()
+    for (u <- 0 to tab.length - 1) {
+      var temp: Double = tab(u)
       res = res ++ Array(temp)
     }
     return res
@@ -179,11 +179,28 @@ object Final {
   def BDDE(tab: Array[Array[Array[Int]]]): Array[Array[Array[Array[Double]]]] = {
 
     var bdde: Array[Array[Array[Array[Double]]]] = Array()
-    for (i <- 0 to tab.length - 1) {
+    var n : Int = tab.length
+    var k : Int = 1
+    for (i <- 0 to n-1) {
+      println("Musique " + k + " sur " + n)
       bdde = bdde ++ Array(empreinte(tab(i)))
-      println("Musique suivante")
+      k = k + 1
     }
     return bdde
+  }
+
+  def MtoC(tab: Array[Array[Double]]): Array[Array[Double]] = {
+    var tabres: Array[Array[Double]] = Array(Array())
+    tabres(0) = tab(0)
+    var res: Array[Double] = Array()
+    for (j <- 1 to tab.length - 1) {
+      res = Array()
+      for (i <- 0 to 511) {
+        res = res ++ Array((tab(j)(2*i) + tab(j)(2*i + 1)) / 2)
+      }
+      tabres = tabres ++ Array(res)
+    }
+    return tabres
   }
 
   def empreinte(tab: Array[Array[Int]]): Array[Array[Array[Double]]] = {
@@ -193,10 +210,13 @@ object Final {
     var t4: Array[Double] = Array()
     var t5: Array[Double] = Array()
     var t6: Array[Double] = Array()
-    var freq: Array[Array[Double]] = fftm(decoupe(downsampling(moyenneStereo(tab))))
+    var test0 : Array[Array[Int]] = moyenneStereo(tab)
+    var test1 : Array[Array[Int]] = downsampling(test0)
+    var test2 : Array[Array[Int]] = decoupe(test1)
+    var freqM: Array[Array[Double]] = fftm(test2)
+    var freq: Array[Array[Double]] = MtoC(freqM)
     var fe: Double = tab(0)(0)
     var tmax: Array[Double] = Array()
-    println("Etape1")
     for (k <- 1 to freq.length - 1) {
       var max: Double = 0
       max = (freq(k).slice(0, 9).reduceLeft(_ max _))
@@ -212,51 +232,64 @@ object Final {
       max = (freq(k).slice(160, 511).reduceLeft(_ max _))
       tmax = tmax ++ Array(max)
     }
-    println("Etape2")
     var mean: Double = 0.0
     for (j <- 0 to 5) {
       mean = mean + tmax(j)
     }
     mean = mean / 6
     var tres: Array[Array[Double]] = Array()
-    var c: Int = 0
-
     for (k <- 0 to freq.length - 1) {
-      var j = 0
-      while (c < 200 && j < freq(k).length - 1) {
-        if (freq(k)(j) > mean*0.5) {
-          tres = tres ++ Array(Array(j * fe / 1024, k + 1))
+    var c: Int = 0
+      var j = -1
+      while ( c < 10 && j < freq(k).length - 1) {
+        j = j + 1
+        if (freq(k)(j) > mean * 0.0) {
+          tres = tres ++ Array(Array( j.toDouble , k + 1))
           c = c + 1
         }
-        j = j + 1
       }
-    }
-    println("Etape4")
+    }  
     var emp: Array[Array[Array[Double]]] = Array()
     for (j <- 0 to tres.length - 8) {
       for (k <- 3 to 7) {
         emp = emp ++ Array(Array(Array(tres(j)(0), tres(j + k)(0), tres(j + k)(1) - tres(j)(1)), Array(tres(j)(1))))
       }
     }
-    println("Empreinte 1 finie")
     return emp
   }
 
   def E_occur(E: Array[Array[Array[Double]]], M: Array[Array[Array[Double]]]): Array[Double] = {
     var T = new Array[Double](0)
-    for (k <- 0 to E.length - 1) {
-
-      for (c <- 0 to M.length - 1) {
-        if (E(k)(0).deep == M(c)(0).deep) {
+    println("Recherche des occurences")
+    for (k <- 0 to E.length - 5) {
+          var test : Int  = 0
+      for (c <- 0 to M.length - 5) {
+        if ((E(k)(0).deep == M(c)(0).deep)) {
+          //test = test + 1
+        //}
+       // if (E(k+1)(0).deep == M(c+1)(0).deep) {
+         //test = test +1 
+        //}
+        //if (E(k+2)(0).deep == M(c+2)(0).deep) {
+          //test = test + 1
+        //}
+        //if (E(k+3)(0).deep == M(c+3)(0).deep) {
+          //test = test + 1
+        //}
+        //if (E(k+4)(0).deep == M(c+4)(0).deep) {
+          //test = test + 1
+        //}
+        //if (test > 3) {
           T = T ++ Array(M(c)(1)(0) - E(k)(1)(0))
         }
+        println( c + " sur " + M.length)
       }
-
     }
     return T
   }
 
   def memediff(T: Array[Double]): Int = {
+    println("Calcul des similitudes")
     var res: Array[Int] = Array.fill(T.length)(0)
     for (i <- 0 to T.length - 1) {
       for (j <- 0 to T.length - 1) {
@@ -280,7 +313,7 @@ object Final {
     var m: Int = 0
     var res: Int = 0
     for (i <- 0 to l.length - 1) {
-      if (l(i) > m) {
+      if (l(i)> m) {
         m = l(i)
         res = i
       }
@@ -288,15 +321,14 @@ object Final {
     return (res);
   }
   def reconnaissance_m(wav2D: Array[Array[Int]], bdde: Array[Array[Array[Array[Double]]]]): String = {
-    Console.out.flush()
     var tab_same: Array[Int] = Array()
-    println("coucou")
     var E: Array[Array[Array[Double]]] = empreinte(wav2D)
     var M: Array[Array[Array[Double]]] = Array()
     for (i <- 0 to bdde.length - 1) {
       M = bdde(i)
       tab_same = tab_same ++ Array(memediff(E_occur(E, M)))
     }
+    //println(E_occur(E,M).deep)
     println(tab_same.deep)
     var indice_musique = indice_max(tab_same)
     return (files(indice_musique))
@@ -309,7 +341,7 @@ object Final {
   }
 
   def main(args: Array[String]): Unit = {
-    var m1 = wav("Je_serai_Mono-11025Hz.wav")
+    var m1 = wav("The_Raiders_March_Mono-11025Hz.wav")
     println("Start")
     var bdde = BDDE(BDD)
     println("Next")
